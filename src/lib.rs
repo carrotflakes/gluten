664b4f29135daab1f3e9dd0107922ed562087c6b
carrotflakes <carrotflakes@gmail.com>
2020-01-26 15:46:42 +0900

    expose string_pool::{StringPool, InternedString}

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ pub mod core;
 #[macro_use]
 pub mod macros;
 
+pub use crate::string_pool::{StringPool, InternedString};
 pub use crate::data::*;
 pub use crate::reader::Reader;
 pub use crate::core::{eval, Env};
