3306134565e3f69172e3fc59a9b4bc919862c32a
carrotflakes <carrotflakes@gmail.com>
2020-02-06 02:59:27 +0900

    impl Eq and Hash for Symbol using pointer

--- src/data.rs
+++ src/data.rs
@@ -1,6 +1,7 @@
 use std::rc::Rc;
 use std::cell::RefCell;
 use std::any::Any;
+use std::hash::{Hash, Hasher};
 use crate::string_pool::InternedString;
 
 pub type R<T> = Rc<RefCell<T>>;
@@ -11,5 +12,19 @@ pub fn r<T: 'static>(t: T) -> Val {
     Rc::new(RefCell::new(t)) as Val
 }
 
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone)]
 pub struct Symbol(pub InternedString);
+
+impl PartialEq for Symbol {
+    fn eq(&self, other: &Self) -> bool {
+        Rc::ptr_eq(&self.0, &other.0)
+    }
+}
+
+impl Eq for Symbol {}
+
+impl Hash for Symbol {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        (&*self.0 as *const String).hash(state);
+    }
+}
