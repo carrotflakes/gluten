e4e813a377894e9932e5cb6f64bf55642aa6a7e8
carrotflakes <carrotflakes@gmail.com>
2020-03-14 20:59:39 +0900

    refactor reader.rs

--- src/reader.rs
+++ src/reader.rs
@@ -1,3 +1,4 @@
+use std::rc::Rc;
 use std::str::Chars;
 use std::iter::Peekable;
 use std::collections::HashMap;
@@ -6,7 +7,7 @@ use crate::string_pool::StringPool;
 use crate::error::GlutenError;
 
 pub type AtomReader = Box<dyn FnMut(&mut StringPool, &str) -> Result<Val, GlutenError>>;
-pub type ReadTable = HashMap<char, &'static dyn for<'a> Fn(&mut Reader, Peekable<Chars<'a>>) -> Result<(Val, Peekable<Chars<'a>>), GlutenError>>;
+pub type ReadTable = HashMap<char, Rc<dyn Fn(&mut Reader, &mut Peekable<Chars>) -> Result<Val, GlutenError>>>;
 
 pub struct Reader {
     pub read_table: ReadTable,
@@ -24,11 +25,12 @@ impl Reader {
     }
 
     pub fn parse(&mut self, src: &str) -> Result<Val, GlutenError> {
-        self.parse_value(src.chars().peekable())
-            .and_then(|mut x| {
-                skip_whitespace(&mut x.1);
-                if x.1.count() == 0 {
-                    Ok(x.0)
+        let mut cs = src.chars().peekable();
+        self.parse_value(&mut cs)
+            .and_then(|val| {
+                skip_whitespace(&mut cs);
+                if let None = cs.peek() {
+                    Ok(val)
                 } else {
                     Err(GlutenError::ReadFailed("expect EOS, but found some character".to_string()))
                 }
@@ -39,10 +41,9 @@ impl Reader {
         let mut vec = Vec::new();
         let mut cs = src.chars().peekable();
         loop {
-            match self.parse_value(cs) {
-                Ok((val, ncs)) => {
+            match self.parse_value(&mut cs) {
+                Ok(val) => {
                     vec.push(val);
-                    cs = ncs;
                     skip_whitespace(&mut cs);
                     if let None = cs.peek() {
                         return Ok(vec);
@@ -55,13 +56,15 @@ impl Reader {
         }
     }
 
-    fn parse_value<'a>(&mut self, mut cs: Peekable<Chars<'a>>) -> Result<(Val, Peekable<Chars<'a>>), GlutenError> {
-        skip_whitespace(&mut cs);
-        if let Some(c) = cs.next() {
+    fn parse_value(&mut self, cs: &mut Peekable<Chars>) -> Result<Val, GlutenError> {
+        skip_whitespace(cs);
+        if let Some(c) = cs.peek() {
             if let Some(f) = self.read_table.get(&c).cloned() {
+                cs.next();
                 f(self, cs)
             } else if !c.is_whitespace() && !['(', ')', '\'', '"', ';'].contains(&c) {
-                let mut vec = vec![c];
+                let mut vec = vec![*c];
+                cs.next();
                 loop {
                     match cs.peek() {
                         Some(c) if !c.is_whitespace() && !['(', ')', '\'', '"', ';'].contains(c) => {
@@ -74,7 +77,7 @@ impl Reader {
                     }
                 }
                 let s: String = vec.iter().collect();
-                (self.atom_reader)(&mut self.string_pool, &s).map(|val| (val, cs))
+                (self.atom_reader)(&mut self.string_pool, &s).map(|val| val)
             } else {
                 Err(GlutenError::ReadFailed(format!("unexpected character: {:?}", c)))
             }
@@ -99,7 +102,7 @@ impl Default for Reader {
 }
 
 
-fn skip_whitespace<'a> (cs: &mut Peekable<Chars<'a>>) {
+fn skip_whitespace (cs: &mut Peekable<Chars>) {
     while let Some(c) = cs.peek() {
         if *c == ';' {
             while match cs.next() {
@@ -120,38 +123,30 @@ pub fn default_atom_reader(sp: &mut StringPool, s: &str) -> Result<Val, GlutenEr
     Ok(r(Symbol(sp.intern(s))))
 }
 
-pub fn read_list<'a>(reader: &mut Reader, mut cs: Peekable<Chars<'a>>) -> Result<(Val, Peekable<Chars<'a>>), GlutenError> {
+pub fn read_list(reader: &mut Reader, cs: &mut Peekable<Chars>) -> Result<Val, GlutenError> {
     let mut vec = vec![];
-    loop {
-        match reader.parse_value(cs.clone()) {
-            Ok((val, ncs)) => {
-                vec.push(val);
-                cs = ncs;
-            },
-            _ => {
-                break;
-            }
-        }
+    while let Ok(val) = reader.parse_value(cs) {
+        vec.push(val);
     }
-    skip_whitespace(&mut cs);
+    skip_whitespace(cs);
     if cs.next() == None {
         return Err(GlutenError::ReadFailed("closing parenthesis missing".to_string()));
     }
-    Ok((r(vec), cs))
+    Ok(r(vec))
 }
 
-pub fn read_quote<'a>(reader: &mut Reader, cs: Peekable<Chars<'a>>) -> Result<(Val, Peekable<Chars<'a>>), GlutenError> {
-    let (val, ncs) = reader.parse_value(cs)?;
+pub fn read_quote(reader: &mut Reader, cs: &mut Peekable<Chars>) -> Result<Val, GlutenError> {
+    let val = reader.parse_value(cs)?;
     let quote = r(Symbol(reader.string_pool.intern("quote")));
-    Ok((r(vec![quote, val]), ncs))
+    Ok(r(vec![quote, val]))
 }
 
-pub fn read_backquote<'a>(reader: &mut Reader, cs: Peekable<Chars<'a>>) -> Result<(Val, Peekable<Chars<'a>>), GlutenError> {
-    let (val, ncs) = reader.parse_value(cs)?;
+pub fn read_backquote(reader: &mut Reader, cs: &mut Peekable<Chars>) -> Result<Val, GlutenError> {
+    let val = reader.parse_value(cs)?;
     let quote = r(Symbol(reader.string_pool.intern("quasiquote")));
-    Ok((r(vec![quote, val]), ncs))
+    Ok(r(vec![quote, val]))
 }
-pub fn read_comma<'a>(reader: &mut Reader, mut cs: Peekable<Chars<'a>>) -> Result<(Val, Peekable<Chars<'a>>), GlutenError> {
+pub fn read_comma(reader: &mut Reader, cs: &mut Peekable<Chars>) -> Result<Val, GlutenError> {
     let op = if cs.peek() == Some(&'@') {
         cs.next();
         "unquote-splicing"
@@ -159,11 +154,11 @@ pub fn read_comma<'a>(reader: &mut Reader, mut cs: Peekable<Chars<'a>>) -> Resul
         "unquote"
     };
     let op = r(Symbol(reader.string_pool.intern(op)));
-    let (val, ncs) = reader.parse_value(cs)?;
-    Ok((r(vec![op, val]), ncs))
+    let val = reader.parse_value(cs)?;
+    Ok(r(vec![op, val]))
 }
 
-pub fn read_string<'a>(_reader: &mut Reader, mut cs: Peekable<Chars<'a>>) -> Result<(Val, Peekable<Chars<'a>>), GlutenError> {
+pub fn read_string(_reader: &mut Reader, cs: &mut Peekable<Chars>) -> Result<Val, GlutenError> {
     let mut vec = Vec::new();
     loop {
         match cs.next() {
@@ -194,15 +189,15 @@ pub fn read_string<'a>(_reader: &mut Reader, mut cs: Peekable<Chars<'a>>) -> Res
         }
     }
     let s: String = vec.iter().collect();
-    Ok((r(s), cs))
+    Ok(r(s))
 }
 
 pub fn make_default_read_table() -> ReadTable {
     let mut rt = ReadTable::new();
-    rt.insert('(', &read_list);
-    rt.insert('\'', &read_quote);
-    rt.insert('`', &read_backquote);
-    rt.insert(',', &read_comma);
-    rt.insert('"', &read_string);
+    rt.insert('(', Rc::new(read_list));
+    rt.insert('\'', Rc::new(read_quote));
+    rt.insert('`', Rc::new(read_backquote));
+    rt.insert(',', Rc::new(read_comma));
+    rt.insert('"', Rc::new(read_string));
     rt
 }
