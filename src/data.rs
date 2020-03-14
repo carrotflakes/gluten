dcecc94e001c61112d8eb95f35c67af6741ebba7
carrotflakes <carrotflakes@gmail.com>
2020-03-15 01:03:01 +0900

    make symbol.rs

--- src/data.rs
+++ src/data.rs
@@ -1,32 +1,13 @@
 use std::rc::Rc;
 use std::any::Any;
-use std::hash::{Hash, Hasher};
-use crate::string_pool::InternedString;
+pub use crate::symbol::Symbol;
 
 pub type R<T> = Rc<T>;
 pub type Val = Rc<dyn Any>;
-pub type MyFn = Box<dyn Fn(Vec<Val>) -> Val>;
+pub type MyFn = Box<dyn Fn(Vec<Val>) -> Val>; // depricated!
+pub type NativeFn = Box<dyn Fn(Vec<Val>) -> Result<Val, crate::error::GlutenError>>;
+pub type SpecialOperator = Box<dyn Fn(&mut crate::core::Env, &Vec<Val>) -> Result<Val, crate::error::GlutenError>>;
 
 pub fn r<T: 'static>(t: T) -> Val {
     Rc::new(t) as Val
 }
-
-#[derive(Debug, Clone)]
-pub struct Symbol(pub InternedString);
-
-impl PartialEq for Symbol {
-    fn eq(&self, other: &Self) -> bool {
-        Rc::ptr_eq(&self.0, &other.0)
-    }
-}
-
-impl Eq for Symbol {}
-
-impl Hash for Symbol {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        (&*self.0 as *const String).hash(state);
-    }
-}
-
-pub type NativeFn = Box<dyn Fn(Vec<Val>) -> Result<Val, crate::error::GlutenError>>;
-pub type SpecialOperator = Box<dyn Fn(&mut crate::core::Env, &Vec<Val>) -> Result<Val, crate::error::GlutenError>>;
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 pub mod string_pool;
+pub mod symbol;
 pub mod data;
 pub mod error;
 pub mod reader;
